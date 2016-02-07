493f0a9f8c5660214cc82bed62ae0e0990146536
hudzspeedz <hudzspeedz@gmail.com>
2016-02-07 15:24:03 +0000

    Initial commit

---
+++ app/src/main/java/com/coventery/jack/activityopen/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.coventery.jack.activityopen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("Key1");
+        TextView aText = (TextView) findViewById(R.id.text1);
+        aText.setText(message);
+    }
+
+}
---
+++ app/src/main/java/com/coventery/jack/activityopen/ThirdActivity.java
@@ -0,0 +1,24 @@
+package com.coventery.jack.activityopen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("Passthrough");
+        TextView aText = (TextView) findViewById(R.id.text2);
+        aText.setText(message);
+    }
+
+}
